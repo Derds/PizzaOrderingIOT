8230bb45e13ca97396d171067eba3a5d9db1efee
Derds <dervlaob@gmail.com>
2020-05-08 15:00:48 +0100

    Skeleton Website
    Added skeleton of website, added basic styling with bootstrap, commented out serial parts for now

--- PizzaCodeInitial/PythonFlask.py
+++ PizzaCodeInitial/PythonFlask.py
@@ -4,32 +4,32 @@
 # Import the flask library
 from flask import Flask, render_template, redirect, url_for
 from threading import Thread
-import serial
+#import serial
 from time import sleep
 
 #arduino will be giving serial read data about the toppings it finds
-serial_data = []
+#serial_data = []
 #open the serial port at this baud rate
-ser = serial.Serial('/dev/ttyUSB0', 115200)
+#ser = serial.Serial('/dev/ttyUSB0', 115200)
 #write a to serial to start reading
-ser.write(b"a")
+#ser.write(b"a")
 
-def get_arduino_data():
-        sleep(1)
+#def get_arduino_data():
+#        sleep(1)
         #write a to serial to start reading
-        ser.write(b"a")
-        while True:
+#        ser.write(b"a")
+#        while True:
                 #TODO add timeout for readline 
                 #https://pyserial.readthedocs.io/en/latest/shortintro.html
-                data = ser.readline().decode()
+#                data = ser.readline().decode()
                 #hacky way to display just the bytes
-                if "epc" in data:
-                        serial_data.append(data.split("epc[")[1].replace("]", ""))
+#                if "epc" in data:
+#                        serial_data.append(data.split("epc[")[1].replace("]", ""))
 
 #threading stuff
-serial_thread = Thread(target=get_arduino_data)
-serial_thread.daemon = True
-serial_thread.start()
+#serial_thread = Thread(target=get_arduino_data)
+#serial_thread.daemon = True
+#serial_thread.start()
 
 #set up website application
 app = Flask(__name__)
@@ -37,6 +37,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
 
 @app.route("/<name>/")
 def user(name):
@@ -48,8 +52,9 @@ def hello():
     #if serial data empty redirect, else display
     if not serial_data:
         return redirect(url_for("home"))
-    else 
-        return "<br />".join(serial_data)
+    #else:
+    #    return "<br />".join(serial_data)
 
 #TODO remove debug = true on final
-app.run(host='0.0.0.0', port=8080, debug=True)
+#app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(debug=True)
