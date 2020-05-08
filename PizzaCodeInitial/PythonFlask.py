0cdbc7b7144699f0cd634ff2d959f58336369d0e
Derds <dervlaob@gmail.com>
2020-05-08 22:19:33 +0100

    Order Pizza on website
    Initial getting pizza order to work

--- PizzaCodeInitial/PythonFlask.py
+++ PizzaCodeInitial/PythonFlask.py
@@ -2,38 +2,26 @@
 #
 
 # Import the flask library
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from threading import Thread
-#import serial
+import serial
 from time import sleep
 
+serial_data = []
 #arduino will be giving serial read data about the toppings it finds
-#serial_data = []
 #open the serial port at this baud rate
-#ser = serial.Serial('/dev/ttyUSB0', 115200)
+ser = serial.Serial('/dev/ttyUSB0', 115200)
 #write a to serial to start reading
-#ser.write(b"a")
-
-#def get_arduino_data():
-#        sleep(1)
-        #write a to serial to start reading
-#        ser.write(b"a")
-#        while True:
-                #TODO add timeout for readline 
-                #https://pyserial.readthedocs.io/en/latest/shortintro.html
-#                data = ser.readline().decode()
-                #hacky way to display just the bytes
-#                if "epc" in data:
-#                        serial_data.append(data.split("epc[")[1].replace("]", ""))
+ser.write(b"a")
 
 #threading stuff
-#serial_thread = Thread(target=get_arduino_data)
-#serial_thread.daemon = True
-#serial_thread.start()
+serial_thread = Thread(target=get_arduino_data)
+serial_thread.daemon = True
+serial_thread.start()
 
 #set up website application
 app = Flask(__name__)
-#Home page
+#set up routing
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -42,19 +30,43 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/<name>/")
-def user(name):
-    return f"Hello {name}!"
+#Order - two methods get and post, if post then the user has clicked "order pizza" button
+#If post request then take a username and redirect to read tags page
+@app.route("/order/", methods=["POST", "GET"])
+def order():
+    if request.method == "POST":
+        #readTags
+        name = request.form["username"]
+        return redirect(url_for("readTags", user = name))
+    else:
+        return render_template("orderPizza.html")
+
 
 #Tags page showing just serial data
-@app.route("/readTags/")    
-def hello():
+@app.route("/readTags/<user>")    
+def readTags(user):
+    #TODO set up default value if username not given
+
     #if serial data empty redirect, else display
+    getPizzaToppings()
+
     if not serial_data:
-        return redirect(url_for("home"))
-    #else:
-    #    return "<br />".join(serial_data)
+        return redirect(url_for("/order"))
+    else:
+        return f"<h1>{user}'s Pizza Order</h1><p></p><br />".join(serial_data)
+
+def getPizzaToppings():
+    sleep(1)
+    #write a to serial to start reading
+    ser.write(b"a")
+    while True:
+        #TODO add timeout for readline 
+        #https://pyserial.readthedocs.io/en/latest/shortintro.html
+        data = ser.readline().decode()
+        #hacky way to display just the bytes
+        if "epc" in data:
+                serial_data.append(data.split("epc[")[1].replace("]", ""))
 
 #TODO remove debug = true on final
-#app.run(host='0.0.0.0', port=8080, debug=True)
-app.run(debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
+#app.run(debug=True)
