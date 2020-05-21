748e45f0f3518b70167722ad5f27e514c9109eb9
Derds <dervlaob@gmail.com>
2020-05-21 19:03:11 +0100

    Update simpleFunctionality.py
    -added timeout for serial data
    -added more exceptions / error catching
    -added debug printlines

--- PizzaCodeInitial/simpleFunctionality.py
+++ PizzaCodeInitial/simpleFunctionality.py
@@ -4,7 +4,7 @@
 # Import the flask library
 from flask import Flask, render_template, redirect, url_for
 from threading import Thread
-import serial
+import serial, time
 from serial import SerialException
 from time import sleep
 
@@ -21,12 +21,13 @@ def page_not_found(e):
 serial_data = []
 errormsg = ""
 try:
-    ser = serial.Serial('/dev/ttyUSB0', 115200)
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout =5)
     ser.write(b"a")
+    print("Serial Port Opened")
 except SerialException as e:
     print("Serial Port Exception:")
     print(e)
-    #return redirect(url_for("/error", msg = errormsg))
+    redirect(url_for('error'))
 
 #error handling
 @app.route("/error/")
@@ -36,17 +37,33 @@ def error():
 
 def get_arduino_stuff():
     try:
-        sleep(1)
+        #sleep(1)
         ser.write(b"a")
+        timeout = time.time() + 30# 30seconds from now
         while True:
             data = ser.readline().decode()
             if "epc" in data:
                     serial_data.append(data.split("epc[")[1].replace("]", ""))
+            #debug print statements
+            if data:
+                print("data")
+                print(data)
+            if serial_data:
+                print("Serial Data:")
+                print(serial_data)
+            
+            if "Module failed to respond. Please check wiring." in data:
+                print(data)
+                raise Exception("Module not wired correctly or power low")
+            #break out of read loop after 30seconds
+            if time.time() > timeout:
+                break
+            
     except Exception as e:
         print("Issue retrieving data:")
         print(type(e))
         print(e)
-        return redirect(url_for("error"))
+        return redirect(url_for('error'))
 
 serial_thread = Thread(target=get_arduino_stuff)
 serial_thread.daemon = True
@@ -60,17 +77,19 @@ def hello():
 @app.route("/list")
 def list():
     try:
+        get_arduino_stuff()
         data = "<br />".join(serial_data)
         if serial_data:
             return data
         else:
+            #TODO could make this cleaner...
             print("Empty data set: no serial data being input")
-            return redirect(url_for("error"))
+            return redirect(url_for('error'))
     except Exception as e:
         print("Issue retrieving data:")
         print(type(e))
         print(e)
-        return redirect(url_for("error"))
+        return redirect(url_for('error'))
 
 app.run(host='0.0.0.0', port=8080, debug=True)
 #app.run( debug=True)
