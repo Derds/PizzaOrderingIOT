8740f75526f32f6d04cf61e8a3ca348ce346dc49
Derds <dervlaob@gmail.com>
2020-05-25 03:11:23 +0100

    Update simpleFunctionality.py
    -attempting to create a clear list of toppings added to the pizza

--- PizzaCodeInitial/simpleFunctionality.py
+++ PizzaCodeInitial/simpleFunctionality.py
@@ -11,46 +11,77 @@ from time import sleep
 # Set up the website
 app = Flask(__name__)
 
-
 #error handling
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
+
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
-    return render_template('404.html'), 404  #todo- make 404 page
+    #return render_template('404.html'), 404  #TODO- make 404 page
+    return render_template(url_for("error"))
 
 serial_data = []
 errormsg = ""
 try:
-    ser = serial.Serial('/dev/ttyUSB0',115200, timeout =5)
+    ser = serial.Serial('/dev/ttyUSB0',115200, timeout =5, bytesize=serial.EIGHTBITS)
     ser.write(b"a")
+    ser.flush()
     print("Serial Port Opened")
 except SerialException as e:
     print("Serial Port Exception:")
     print(e)
-    redirect(url_for('error'))
+    #redirect(url_for("error"))
 
 #error handling
 @app.route("/error/")
 def error():
-        return render_template('error.html', message=errormsg)
+    #return render_template("error.html", message=errormsg)
+    return render_template("error.html")
+
+#toppings_set = {}
 
 
 def get_arduino_stuff():
+    cleanData= ""
+    
+    #create a set for toppings, this means that it will only hold unique values, unlike a list
+    toppings_set = {}
+    toppings_set = set()
     try:
-        #sleep(1)
         ser.write(b"a")
+        sleep(1)
+        ser.flush()
         timeout = time.time() + 15# 15seconds from now
         while True:
-            data = ser.readline().decode()
+            try:
+                data = ser.read(200).decode()
+                #data = ser.read(150).decode()
+                #ser_bytes = ser.readline() #remove trailing newline
+                #data = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+                #data = line.decode("utf-8") #not ascii
+                #data = decoded_bytes
+            except UnicodeDecodeError:
+                print("decoding error")
             if "epc" in data:
-                    serial_data.append(data.split("epc[")[1].replace("]", ""))
+                
+                cleanData = data.split("]")[len(data.split("]"))]#just the text
+                serial_data.append(data.split("epc[")[1].replace("]", ""))
+                toppings_set.add(cleanData)
             #debug print statements
             if data:
-                print("data")
+                print("data:")
                 print(data)
+            if cleanData:
+                print("clean:")
+                print(cleanData)
             if serial_data:
                 print("Serial Data:")
                 print(serial_data)
+            if toppings_set:
+                print(toppings_set)
             
             if "Module failed to respond. Please check wiring." in data:
                 print(data)
@@ -63,7 +94,7 @@ def get_arduino_stuff():
         print("Issue retrieving data:")
         print(type(e))
         print(e)
-        return redirect(url_for('error'))
+        #return redirect(url_for("error"))
 
 serial_thread = Thread(target=get_arduino_stuff)
 serial_thread.daemon = True
@@ -93,3 +124,35 @@ def list():
 
 app.run(host='0.0.0.0', port=8080, debug=True)
 #app.run( debug=True)
+
+##TODO: test this 
+# def getPizzaToppings():
+#     sleep(1)
+#     #write a to serial to start reading
+#     ser.write(b"a")
+#     while True:
+#         try:
+#             #TODO add timeout for readline 
+#             #serial = serial.Serial("/dev/ttyUSB0", 9600, timeout=1) #timeout for if it cant find within that time.
+#             #https://pyserial.readthedocs.io/en/latest/shortintro.html
+#             ser_bytes = ser.readline()
+#             decoded_bytes = ser_bytes.decode()
+#             print(decoded_bytes)
+#             with open("toppings_data.csv","a") as f:  #find 
+#                 writer = csv.writer(f,delimiter=",")
+#                 writer.writerow([time.time(),decoded_bytes])
+#             #hacky way to display just the bytes
+#             #if "epc" in data:
+#             #    serial_data.append(data.split("epc[")[1].replace("]", ""))
+#         except:
+#             print("Keyboard Interrupt")
+#             break
+
+#Python Dynamic List>
+# x = []
+# n = input("enter length")
+# for i in range(1, int(n)):
+#     k=input("enter value")
+#     x.append(k) # push your entered value
+
+# print x
